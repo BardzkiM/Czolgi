be945eca35f4482b36f912d6eef9eef1e837c7b7
olaboga13 <anna.gawor.13@gmail.com>
2016-05-11 19:21:47 +0200

    align headers

--- Project1/Project1/Menu.h
+++ Project1/Project1/Menu.h
@@ -13,34 +13,21 @@ class Menu
 public:
 	Menu();
 	~Menu();
+
 	int position;
-
-	int set_bg(sf::RenderWindow * window);
-
-	int set_menu_pos_1(sf::RenderWindow * window);
-
-	int set_menu_pos_2(sf::RenderWindow * window);
-
-	int set_menu_pos_3(sf::RenderWindow * window);
-
-	int set_auto_menu_pos(sf::RenderWindow * window);
-
-	void key_up(sf::RenderWindow * window);
-
-	void key_down(sf::RenderWindow * window);
-
-
-
 	sf::Texture menu_bg;
-
 	sf::Texture menu_bg_bullets;
-
 	sf::SoundBuffer buffer;
 	sf::Sound sound;
 	sf::Music music;
 	
-
-
+	int set_bg(sf::RenderWindow * window);
+	int set_menu_pos_1(sf::RenderWindow * window);
+	int set_menu_pos_2(sf::RenderWindow * window);
+	int set_menu_pos_3(sf::RenderWindow * window);
+	int set_auto_menu_pos(sf::RenderWindow * window);
+	void key_up(sf::RenderWindow * window);
+	void key_down(sf::RenderWindow * window);
 
 private:
 	
